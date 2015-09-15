3de85b9fe8359cba4d4a6938e78f1a4b9da3f4ff
Rodrigo <Rodrigo@RodrigoNotebook>
2015-09-15 00:09:14 -0300

    Adicionado a busca de coord de maior valor e rascunho do minimax.

--- src/Minimax.java
+++ src/Minimax.java
@@ -26,14 +26,43 @@ public class Minimax {
      * @param beta      Melhor movimento do Min
      * @param depth     Profundidade a procurar
      * @param player    Jogador atual
+     * @return a melhor pontuacao encontrada
+     * IA é o Max
+     * Jogador é o Min
+
      */
     
 
-    public void MiniMax(Tabuleiro board, int alfa, int beta, int depth, Jogador player){
-//        if (board.temCincoPecasEmSequencia()){
-//            Jogador ganhador = new Jogador(tabuleiro.getWinner());
-//            terminaJogo;
-//        }
+    public double minimax(Tabuleiro board, double alfa, double beta, int depth, EnumTabuleiro player){
         
+        double pontuacao = 0;
+        int melhorColuna = 99;
+        int melhorLinha = 99;
+        
+        if (depth == 0) {
+            if (board.getSomaAmeaca() == 0) {
+                melhorColuna = 7;
+                melhorLinha = 7;
+            } else {
+                
+            }
+        } else {
+            if (player.equals(EnumTabuleiro.IA)) {
+                pontuacao = minimax(board, alfa, beta, depth - 1, EnumTabuleiro.JOGADOR); //Tem que ser jogador pois o proximo nivel sera Min
+                if (pontuacao > alfa) {
+                    alfa = pontuacao;
+                }
+            } else {
+                pontuacao = minimax(board, alfa, beta, depth - 1, EnumTabuleiro.IA); //Tem que ser IA pois o proximo nivel sera Max
+                if (pontuacao < beta) {
+                    beta = pontuacao;
+                }
+            }
+        }
+        
+        
+        
+        
+        return 0; //so pro compilador parar de reclamar.
     }
 }
